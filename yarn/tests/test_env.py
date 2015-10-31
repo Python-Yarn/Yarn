1ec2b70e98739a3e665694aab690ea47a2b8b6a3
Jason L McFarland <Jason.McFarland1976+python-yarn@gmail.com>
2015-10-31 09:07:58 -0400

    Adding an assertion message for debugging

--- yarn/tests/test_env.py
+++ yarn/tests/test_env.py
@@ -4,8 +4,6 @@ import os
 import unittest
 from yarn.api import env
 
-print(dir(env))
-
 class TestEnv(unittest.TestCase):
 
     def test_env_port(self):
@@ -15,30 +13,18 @@ class TestEnv(unittest.TestCase):
     def test_env_port_non_int(self):
         with self.assertRaises(AttributeError):
             env.host_port = "test"
-#        def set_port_non_int():
-#            env.host_port = "test"
-#        self.assertRaises(AttributeError, set_port_non_int)
 
     def test_env_port_bad_int(self):
         with self.assertRaises(AttributeError):
             env.host_port = 111111
-#        def set_port_bad_int():
-#            env.host_port = 111111
-#        self.assertRaises(AttributeError, set_port_bad_int)
 
     def test_env_user_autoset(self):
-        assert env.user == os.environ["USERNAME"]
+        assert env.user == os.environ["USERNAME"], "Didn't get correct user.  ENV: {}".format(os.environ)
 
     def test_env_username_bad_input(self):
         with self.assertRaises(AttributeError):
             env.user = 12
-#        def set_bad_username():
-#            env.user = 12
-#        self.assertRaises(AttributeError, set_bad_username)
 
     def test_env_password_bad_input(self):
         with self.assertRaises(AttributeError):
             env.password = 12
-#        def set_bad_password():
-#            env.password = 12
-#        self.assertRaises(AttributeError, set_bad_password)
