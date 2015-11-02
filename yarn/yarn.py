c9cebfe3bc8f88eb4b6f482dd4d8b791f5ab34d5
Jason L McFarland <Jason.McFarland1976+python-yarn@gmail.com>
2015-11-01 20:25:58 -0500

    Added host:port checking and cleaned up execution

--- yarn/yarn.py
+++ yarn/yarn.py
@@ -13,42 +13,53 @@ else:
 def parse_host_list(host_list):
     return host_list.split(",")
 
+
 def parse_yarn_file_path(yarnfile):
     return os.path.abspath(yarnfile)
 
 
-def execute(tasks, command, run_parallel=False):
-    @parallel
-    def parallel_execution(*args, **kwargs):
-        tasks = kwargs['tasks']
-        command = args[0]
-        try:
-            tasks[command]()
-        except KeyError:
-            print(run(command))
-
-    def serial_execution(*args, **kwargs):
-        tasks = kwargs['tasks']
-        command = args[0]
-        try:
-            tasks[command]()
-        except KeyError:
-            print(run(command))
-
-    if run_parallel:
-        parallel_execution(command, tasks=tasks)
+@parallel
+def execute_task(*args, **kwargs):
+    tasks = args[0]
+    command = args[1]
+    if command in tasks:
+        print(tasks[command]())
     else:
-        serial_execution(command, tasks=tasks)
+        print(run(command))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hosts', '-H', type=parse_host_list, help="Host(s) separated by commas")
-    parser.add_argument('--user', '-U', type=str, help="Username to use for connecting, defaults to current username")
-    parser.add_argument('--parallel', '-P', dest="parallel", action="store_true", help="Run commands in parallel", default=False)
-    parser.add_argument('--yarn-file', '-f', type=parse_yarn_file_path, help="Yarn file to use, defaults to yarnfile.py", default="./yarnfile.py")
-    parser.add_argument('--warn-only', '-w', action="store_true", dest="warn_only", help="Do not halt upon finding an error", default=False)
-    parser.add_argument('--quiet', '-q', action="store_true", dest="quiet", help="Suppress extra output.", default=False)
+    parser.add_argument('--hosts', '-H',
+                        type=parse_host_list,
+                        help="Host(s) separated by commas.")
+    parser.add_argument('--user', '-U',
+                        type=str,
+                        help="Username to use for connecting, defaults to current username.")
+    parser.add_argument('--parallel', '-P',
+                        dest="parallel",
+                        action="store_true",
+                        help="Run commands in parallel.",
+                        default=False)
+    parser.add_argument('--port', '-p',
+                        dest="port",
+                        type=int,
+                        help="What port to use for connecting to the remote host. Defaults to 22",
+                        default=22)
+    parser.add_argument('--yarn-file', '-f',
+                        type=parse_yarn_file_path,
+                        help="Yarn file to use, defaults to yarnfile.py.",
+                        default="./yarnfile.py")
+    parser.add_argument('--warn-only', '-w',
+                        action="store_true",
+                        dest="warn_only",
+                        help="Do not halt upon finding an error.",
+                        default=False)
+    parser.add_argument('--quiet', '-q',
+                        action="store_true",
+                        dest="quiet",
+                        help="Suppress extra output.",
+                        default=False)
     parser.add_argument(nargs='+', dest='commands')
 
     args = parser.parse_args()
@@ -62,17 +73,23 @@ def main():
         env.user = args.user
     env.quiet = args.quiet
     env.warn_only = args.warn_only
+    env.run_parallel = args.parallel
     if args.hosts:
         for host in args.hosts:
             env.host_string = host
             if "@" in host:
                 # This is to handle the ability to have username@hostname on the command line
                 env.user, env.host_string = host.split("@")
+            if ":" in env.host_string:
+                # This is to handle the ability to have hostname:port on the command line
+                host_string, port = env.host_string.split(":")
+                env.host_string = host_string
+                env.port = int(port)
             for command in args.commands:
-                execute(tasks, command, args.parallel)
+                execute_task(tasks, command)
     else:
         for command in args.commands:
-            execute(tasks, command, args.parallel)
+            execute_task(tasks, command)
 
 
 
